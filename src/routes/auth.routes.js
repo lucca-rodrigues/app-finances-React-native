e4d76e8067a065d738e295c9df742f9ab208b23e
admin <admin@MacBook-Pro-de-admin.local>
2021-09-03 12:45:32 -0300

    add configs prev page header

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -10,7 +10,17 @@ function AuthRoutes() {
   return (
     <AuthStack.Navigator>
       <AuthStack.Screen name="SignIn" component={SignIn} options={{headerShown: false}}/>
-      <AuthStack.Screen name="SignUp" component={SignUp} options={{headerShown: false}}/>
+      <AuthStack.Screen name="SignUp" component={SignUp} options={{
+                    headerStyle: {
+                        backgroundColor: '#131313',
+                        borderBottomColor: '#00b94a',
+                        borderBottomWidth: 1,
+                    },
+                    headerTintColor: 'rgba(255,255,255, 0.5)',
+                    headerBackTitleVisible: false,
+                    headerTitle: 'Voltar'
+
+                }}/>
     </AuthStack.Navigator>
   );
 }
